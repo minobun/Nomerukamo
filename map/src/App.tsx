9971558bb97fc94fb644cf32497a5a3c4a346d36
Naoya Minomura <naotoitoi@gmail.com>
2023-12-03 22:18:20 +0900

    add office locations

--- map/src/App.tsx
+++ map/src/App.tsx
@@ -1,17 +1,41 @@
-import Map from 'react-map-gl/maplibre';
+import Map, { Source, Layer } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import type { FeatureCollection } from 'geojson';
+import type { CircleLayer } from 'react-map-gl/maplibre';
+
+const geojson: FeatureCollection = {
+  "type": "FeatureCollection",
+  "features": [
+    { "type": "Feature", "geometry": { "type": "Point", "coordinates": [139.6946632, 35.5300397] }, "properties": { "name": "川崎タワー" } },
+    { "type": "Feature", "geometry": { "type": "Point", "coordinates": [139.6996434, 35.5349831] }, "properties": { "name": "ソリッドスクエア" } }
+  ]
+}
+const officeLayerStyle: CircleLayer = {
+  "id": "offices",
+  "type": "circle",
+  "source": "office",
+  "paint": {
+    "circle-color": "blue"
+  }
+};
 
 function App() {
+
+
   return (
     <Map
       initialViewState={{
-        longitude: 139.69696941,
-        latitude: 35.53152244,
+        longitude: 139.69696947,
+        latitude: 35.53152243,
         zoom: 16
       }}
       style={{ width: '100vw', height: '100vh' }}
       mapStyle="https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json"
-    />
+    >
+      <Source id="office" type="geojson" data={geojson}>
+        <Layer {...officeLayerStyle} />
+      </Source>
+    </Map>
   )
 }
 
