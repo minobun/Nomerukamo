8b070c80c22160f5809d71c830d7b821305ab388
Naoya Minomura <naotoitoi@gmail.com>
2023-12-03 19:49:16 +0900

    make initial map

--- map/src/App.tsx
+++ map/src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Map from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Map
+      initialViewState={{
+        longitude: 139.69696941,
+        latitude: 35.53152244,
+        zoom: 16
+      }}
+      style={{ width: '100vw', height: '100vh' }}
+      mapStyle="https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json"
+    />
   )
 }
 
