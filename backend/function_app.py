25200d3b9c228b9bf753ece504ae9e447bd53cc6
Naoya Minomura <naotoitoi@gmail.com>
2023-12-07 22:58:25 +0900

    fix syntax

--- backend/function_app.py
+++ backend/function_app.py
@@ -8,7 +8,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    api_token = os.environ('AzureAPI')
+    api_token = os.environ['AzureAPI']
 
     # パラメータの構築
     params = {'address': '神奈川県川崎市幸区大宮町', 'key': api_token}
