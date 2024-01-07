c5b5995be99437446c743d159c892ad37baea483
Naoya Minomura <naotoitoi@gmail.com>
2024-01-07 16:17:11 +0900

    fix backend

--- backend/function_app.py
+++ backend/function_app.py
@@ -6,21 +6,26 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     import os
-    import requests
+    from urllib import request
+    from urllib import parse
+    import json
     
     logging.info('Python HTTP trigger function processed a request.')
 
     api_token = os.environ['AzureAPI']
 
     # パラメータの構築
-    params = {'input': '神奈川県川崎市幸区大宮町', 'key': api_token, 'inputtype': 'textquery'}  # 修正
+    params = {'input': '神奈川県川崎市幸区大宮町', 'key': api_token, 'inputtype': 'textquery'}
+    query = parse.urlencode(params)
 
     # Geocoding APIへのリクエスト
-    res = requests.get("https://maps.googleapis.com/maps/api/place/findplacefromtext/json", params=params)  # 修正
+    response = request.urlopen("https://maps.googleapis.com/maps/api/place/findplacefromtext/json" + query)
+
+    print(response)
 
     # レスポンスの解析
-    if res.status_code == 200:
-        data = res.json()
+    if response.status_code == 200:
+        data = response.json()
         return func.HttpResponse(data)
     else:
         return func.HttpResponse("Error: {}".format(res.status_code), status_code=res.status_code)
