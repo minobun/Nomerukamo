5de7f042c925bda41b2917c4a2601fa198e0b32e
Naoya Minomura <naotoitoi@gmail.com>
2024-03-03 16:48:40 +0900

    render mui when ssg/ssr

--- webapp/src/pages/_document.tsx
+++ webapp/src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import { DocumentHeadTags, DocumentHeadTagsProps, documentGetInitialProps } from "@mui/material-nextjs/v14-pagesRouter";
+import { DocumentContext, Head, Html, Main, NextScript } from "next/document";
 
-export default function Document() {
+export default function Document(props: DocumentHeadTagsProps) {
   return (
     <Html lang="en">
       <Head />
+      <DocumentHeadTags {...props} />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
+}
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  return await documentGetInitialProps(ctx)
 }
